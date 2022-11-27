1d16878c0fa871bacd617c5ed110a246934bcf8c
KGW2027 <retryer_@naver.com>
2022-11-27 15:08:01 +0900

    refactor: Overall의 정보 새로고침 기능을 IUIBuilder로 분리

--- Assets/scripts/IP/Control/OverallManager.cs
+++ Assets/scripts/IP/Control/OverallManager.cs
@@ -38,17 +38,7 @@ namespace IP.Control
 
         private void UpdateInfoTexts()
         {
-            _infoTexts["NAME"].text = GameManager.Instance.GetCompanyName();
-            _infoTexts["STARTDATE"].text = $"창립 일자 : {GameManager.Instance.GetStartDate()[0]:000}Y {GameManager.Instance.GetStartDate()[1]:00}M";
-            _infoTexts["CONNCOUNTRY"].text = $"연결된 국가 : {GameManager.Instance.GetConnectCountries()} / 5";
-            _infoTexts["CONNCITY"].text = $"연결된 도시 : {GameManager.Instance.GetConnectCities()} / 50";
-            _infoTexts["BUILDS"].text = $"설치한 건물 수 : {GameManager.Instance.GetBuilds():n0}";
-            _infoTexts["CUSTOMERS"].text = $"총 소비자 수 : {GameManager.Instance.GetCustomers():n0}";
-            _infoTexts["MONEY"].text = $"보유 자금 : {GameManager.Instance.GetHaveMoney():n0}";
-            _infoTexts["MONTHMONEY"].text = $"월 수익 : -";
-            _infoTexts["DEBT"].text = $"총 부채 : {GameManager.Instance.GetDebt():n0}";
-            _infoTexts["USEMONEY"].text = "월 지출 : -";
-            _infoTexts["CHANGEMONEY"].text = "총 변동 : -";
+            infoTextsParent.GetComponent<OverallInfo>().Build();
         }
 
         private void UpdateBuildsInfo()
---
+++ Assets/scripts/IP/UIFunc/Builder/OverallInfo.cs
@@ -0,0 +1,40 @@
+﻿using TMPro;
+using UnityEngine;
+
+namespace IP.UIFunc.Builder
+{
+    public class OverallInfo : MonoBehaviour, IUIBuilder
+    {
+        public GameObject companyName;
+        public GameObject startDate;
+        public GameObject connCountries;
+        public GameObject connCities;
+        public GameObject qtyBuilds;
+        public GameObject qtyCustomers;
+        public GameObject money;
+        public GameObject monthMoney;
+        public GameObject debt;
+        public GameObject useMoney;
+        public GameObject changeMoney;
+        
+        public void Build()
+        {
+            SetText(companyName, GameManager.Instance.GetCompanyName());
+            SetText(startDate, $"창립 일자 : {GameManager.Instance.GetStartDate()[0]:000}Y {GameManager.Instance.GetStartDate()[1]:00}M");
+            SetText(connCountries, $"연결된 국가 : {GameManager.Instance.GetConnectCountries()} / 5");
+            SetText(connCities, $"연결된 도시 : {GameManager.Instance.GetConnectCities()} / 50");
+            SetText(qtyBuilds, $"설치한 건물 수 : {GameManager.Instance.GetBuilds():n0}");
+            SetText(qtyCustomers, $"총 소비자 수 : {GameManager.Instance.GetCustomers():n0}");
+            SetText(money, $"보유 자금 : {GameManager.Instance.GetHaveMoney():n0}");
+            SetText(monthMoney, $"월 수익 : -");
+            SetText(debt, $"총 부채 : {GameManager.Instance.GetDebt():n0}" );
+            SetText(useMoney, "월 지출 : -");
+            SetText(changeMoney, "총 변동 : -");
+        }
+
+        private void SetText(GameObject go, string text)
+        {
+            go.GetComponent<TextMeshProUGUI>().text = text;
+        }
+    }
+}
