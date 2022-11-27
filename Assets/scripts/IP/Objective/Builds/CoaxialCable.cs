f8c308883e404fb64c9339dcc5f80d6de72df4d1
KGW2027 <retryer_@naver.com>
2022-11-27 14:08:18 +0900

    feat: Build 목록에 동축케이블 추가

---
+++ Assets/scripts/IP/Objective/Builds/CoaxialCable.cs
@@ -0,0 +1,51 @@
+﻿using UnityEngine;
+
+namespace IP.Objective.Builds
+{
+    public class CoaxialCable : MonoBehaviour, IBuild
+    {
+        public Texture thumbnail;
+
+        private City _city;
+        private bool _complete = false;
+        public string GetName()
+        {
+            return "동축 케이블";
+        }
+
+        public int GetBuildDate()
+        {
+            return 1;
+        }
+
+        public long GetBudget()
+        {
+            return 100;
+        }
+
+        public City GetCity()
+        {
+            return _city;
+        }
+
+        public void SetCity(City city)
+        {
+            _city = city;
+        }
+
+        public Texture GetTexture()
+        {
+            return thumbnail;
+        }
+
+        public bool IsComplete()
+        {
+            return _complete;
+        }
+
+        public void Complete()
+        {
+            _complete = true;
+        }
+    }
+}
