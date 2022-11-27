db9fe4a6a087c8f549ed275e16b50c743040efb9
KGW2027 <retryer_@naver.com>
2022-11-27 16:25:44 +0900

    refactor: UIText를 수정하는 static 함수 추가

---
+++ Assets/scripts/IP/StaticFunctions.cs
@@ -0,0 +1,18 @@
+﻿using TMPro;
+using UnityEngine;
+
+namespace IP
+{
+    public static class StaticFunctions
+    {
+        public static void SetUIText(GameObject obj, string text)
+        {
+            obj.GetComponent<TextMeshProUGUI>().text = text;
+        }
+        
+        public static void SetText(GameObject obj, string text)
+        {
+            obj.GetComponent<TextMeshPro>().text = text;
+        }
+    }
+}
