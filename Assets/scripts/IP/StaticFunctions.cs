41e820d0cd92e0026976d973eb7ea8ecd395f265
KGW2027 <retryer_@naver.com>
2022-11-29 23:06:42 +0900

    refactor: StaticFunctions가 TMP 컴포넌트를 기록하도록 변경

--- Assets/scripts/IP/StaticFunctions.cs
+++ Assets/scripts/IP/StaticFunctions.cs
@@ -1,23 +1,30 @@
-﻿using TMPro;
+﻿using System.Collections.Generic;
+using TMPro;
 using UnityEngine;
 
 namespace IP
 {
     public static class StaticFunctions
     {
+        private static Dictionary<GameObject, TextMeshProUGUI> _uiTexts = new();
+        private static Dictionary<GameObject, TextMeshPro> _texts = new();
+
         public static void SetUIText(this GameObject obj, string text)
         {
-            obj.GetComponent<TextMeshProUGUI>().text = text;
+            if (!_uiTexts.ContainsKey(obj)) _uiTexts[obj] = obj.GetComponent<TextMeshProUGUI>();
+            _uiTexts[obj].text = text;
         }
         
         public static void SetText(this GameObject obj, string text)
         {
-            obj.GetComponent<TextMeshPro>().text = text;
+            if (!_texts.ContainsKey(obj)) _texts[obj] = obj.GetComponent<TextMeshPro>();
+            _texts[obj].text = text;
         }
 
         public static void SetUITextColor(this GameObject obj, Color color)
         {
-            obj.GetComponent<TextMeshProUGUI>().color = color;
+            if (!_uiTexts.ContainsKey(obj)) _uiTexts[obj] = obj.GetComponent<TextMeshProUGUI>();
+            _uiTexts[obj].color = color;
         }
     }
 }
