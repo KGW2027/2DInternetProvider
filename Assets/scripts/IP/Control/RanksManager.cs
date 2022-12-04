bd5768eb0fed101db9c7daf11a07d2841efc255e
KGW2027 <retryer_@naver.com>
2022-12-05 00:20:25 +0900

    feat: 회사들의 순위를 점유율순으로 정렬

--- Assets/scripts/IP/Control/RanksManager.cs
+++ Assets/scripts/IP/Control/RanksManager.cs
@@ -1,3 +1,5 @@
+using System.Collections.Generic;
+using System.Linq;
 using IP.AI;
 using IP.Objective;
 using IP.UIFunc.Builder;
@@ -7,6 +9,7 @@ namespace IP.Control
 {
     public class RanksManager : MonoBehaviour
     {
+        
     
         [Header("회사 정보 프리팹")]
         public GameObject panel;
@@ -30,20 +33,34 @@ namespace IP.Control
             float x = 960;
             float y = -60;
 
-            GameObject userPanel = Instantiate(panel, transform, true);
-            UpdatePanelDisplay(userPanel, GameManager.Instance.Company);
+            // 등수 매기기
+            Dictionary<Company, long> rank = new Dictionary<Company, long>();
+            
+            // 유저 회사 등록
+            Company userCompany = GameManager.Instance.Company;
+            rank[userCompany] = userCompany.GetTotalCustomers();
 
+            // AI 회사 등록
             foreach (Company aiCompany in AIManager.Instance.Companies)
             {
-                GameObject aiPanel = Instantiate(panel, transform, true);
-                UpdatePanelDisplay(aiPanel, aiCompany);
+                rank[aiCompany] = aiCompany.GetTotalCustomers();
+            }
+
+            // 정렬 후 프린트
+            using var sortedDict = (from entry in rank orderby entry.Value descending select entry).GetEnumerator();
+            int rankNum = 1;
+            while (sortedDict.MoveNext())
+            {
+                Debug.Log($"[{rankNum}] {sortedDict.Current.Key.Name} -> 수익 : {sortedDict.Current.Key.CalcRevenue()} / 소비자 수 : {sortedDict.Current.Key.GetTotalCustomers()}");
+                GameObject rankPanel = Instantiate(panel, transform, true);
+                UpdatePanelDisplay(rankPanel, sortedDict.Current.Key, rankNum++);
             }
         }
 
-        private void UpdatePanelDisplay(GameObject target, Company company)
+        private void UpdatePanelDisplay(GameObject target, Company company, int rank)
         {
             RankPanelInfo rpi = target.GetComponent<RankPanelInfo>();
-            rpi.SendData(company, 1);
+            rpi.SendData(company, rank);
             rpi.Build();
         }
     }
