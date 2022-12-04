addbd782022b395dd2ca90e7f1a8641332b9b14b
KGW2027 <retryer_@naver.com>
2022-12-04 22:59:36 +0900

    feat: 새로운 요금제 추가 및 수정 기능 추가

--- Assets/scripts/IP/Control/PlanManager.cs
+++ Assets/scripts/IP/Control/PlanManager.cs
@@ -1,4 +1,5 @@
-﻿using IP.Objective;
+﻿using System;
+using IP.Objective;
 using IP.UIFunc.Builder;
 using TMPro;
 using UnityEngine;
@@ -15,10 +16,14 @@ namespace IP.Control
         public TMP_InputField planBandwidth;
         public TMP_InputField planUpload;
         public TMP_InputField planDownload;
+
+        private bool _isEditMode;
+        private PaymentPlan _editingPlan;
         
         public void UpdateUI()
         {
             ClearChild();
+            ResetPlan();
             foreach (PaymentPlan plan in GameManager.Instance.Company.PlanList)
             {
                 var planPanel = Instantiate(planPrefab, planList.transform, true);
@@ -38,12 +43,36 @@ namespace IP.Control
 
         public void CreatePlan()
         {
+            PaymentPlan plan = new PaymentPlan(GameManager.Instance.Company);
+            plan.Name = planName.text;
+            plan.Budget = Convert.ToInt64(planBudget.text);
+            plan.Bandwidth = Convert.ToInt64(planBandwidth.text) * StaticFunctions.Bytes.GB;
+            plan.Upload = Convert.ToInt64(planUpload.text);
+            plan.Download = Convert.ToInt64(planDownload.text);
             
+            if (_isEditMode)
+            {
+                _editingPlan.Cities.ForEach(city => plan.Service(city));
+                GameManager.Instance.Company.DeletePlan(_editingPlan);
+                
+                _isEditMode = false;
+                _editingPlan = null;
+            }
+            
+            GameManager.Instance.Company.AddPlan(plan);
+
+            UpdateUI();
         }
 
         public void Edit(PaymentPlan plan)
         {
-            
+            planName.text = plan.Name;
+            planBudget.text = plan.Budget.ToString();
+            planBandwidth.text = (plan.Bandwidth / StaticFunctions.Bytes.GB).ToString();
+            planUpload.text = plan.Upload.ToString();
+            planDownload.text = plan.Download.ToString();
+            _isEditMode = true;
+            _editingPlan = plan;
         }
 
         public void Open(PaymentPlan plan)
@@ -53,6 +82,8 @@ namespace IP.Control
 
         public void ResetPlan()
         {
+            _isEditMode = false;
+            _editingPlan = null;
             planName.text = "";
             planBudget.text = "";
             planBandwidth.text = "";
