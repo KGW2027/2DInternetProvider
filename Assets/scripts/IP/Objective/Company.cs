c3bc82d572792a203548eedff5e380a55287c434
KGW2027 <retryer_@naver.com>
2022-12-05 23:00:50 +0900

    fix: 건설 소모 비용이 잘못된 함수를 참조하고 있던 부분 수정

--- Assets/scripts/IP/Objective/Company.cs
+++ Assets/scripts/IP/Objective/Company.cs
@@ -132,7 +132,7 @@ namespace IP.Objective
             int[] nowDate = AppBarManager.Instance.GetDate();
             GetUnderConstructBuilds().ForEach(build =>
             {
-                use += (long) build.GetBudget();
+                use += (long) build.GetUseBudget();
                 if (build.GetEndDate()[0] >= nowDate[0] && build.GetEndDate()[1] >= nowDate[1])
                 {
                     build.Complete(this);
